762d03215a36549183df0312fe816dbac5307d8a
Ricardo Santos da Silva <34078950+rick-ssa@users.noreply.github.com>
2020-08-16 22:07:15 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,14 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+class RemoveZeros(BaseEstimator, TransformerMixin):
+    def fit(self, df, y=None):
+        return self
+    
+    def transform(self, df):
+        ndf = df.loc[df["NOTA_DE"]!=0]
+        ndf = df.loc[df["NOTA_EM"]!=0]
+        ndf = df.loc[df["NOTA_MF"]!=0]
+        ndf = df.loc[df["NOTA_GO"]!=0]
+        return ndf
